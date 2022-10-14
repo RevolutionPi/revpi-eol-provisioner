4b39b934fd48034f1ece57604e180b8852ac9e48
Holger Jorra <h.jorra@kunbus.com>
2022-10-14 15:10:12 +0200

    Added simple skeleton to main app
    Takes three parameters: product number, MAC, file path.
    All parameters are forwarded to existing mocks.

--- provisioner.py
+++ provisioner.py
@@ -5,24 +5,47 @@ from revpi_provisioning.network import find_interface_class
 from revpi_provisioning.network.usb import LAN95XXNetworkInterface
 from revpi_provisioning.revpi import RevPi
 
-####################### FROM TEMPLATE - BEGIN################################
 
-# define product
-revpi = RevPi(100359, 1)
+def _extract_product(product_number: str) -> tuple:
+    product_id = int(product_number[2:8])
+    revision = int(product_number[9:11])
+    return (product_id, revision)
 
-# define HAT EEPROM
-revpi.hat_eeprom = HatEEPROM(22)
 
-# define network interfaces (with attached EEPROMs)
-revpi.network_interfaces.append(LAN95XXNetworkInterface("1-5.1.2:1.0", True))
-revpi.network_interfaces.append(LAN95XXNetworkInterface("1-5.1.2:1.0", True))
+if __name__ == "__main__":
+    import argparse
 
-# ... or define network interfaces and determine class from type string (eg. with yaml templates or arparse)
-LAN = find_interface_class("lan87xx")
-revpi.network_interfaces.append(LAN('0000:00:14.3', True))
+    parser = argparse.ArgumentParser(description="Provision RevPi hardware")
+    parser.add_argument('product_number', metavar='product-number',
+                        help='product number of target device in format PRxxxxxxRxx')
+    parser.add_argument('mac_address', metavar='mac-address',
+                        help='first MAC address of target device')
+    parser.add_argument('eep_image', metavar='eep-image',
+                        help='path to eep-image file to be written')
 
-####################### FROM TEMPLATE - END #################################
+    args = parser.parse_args()
+    product = args.product_number
+    mac = args.mac_address
+    image_path = args.eep_image
 
-# device specific parts (eg. in a loop)
-revpi.write_hat_eeprom("revpi-hat-PR100362R01.bin")
-print(revpi.write_mac_addresses('c83ea7001234'))
+    _extract_product(product)
+    ####################### FROM TEMPLATE - BEGIN #############################
+    # define product
+    revpi = RevPi(*_extract_product(product))
+
+    # define HAT EEPROM
+    revpi.hat_eeprom = HatEEPROM(22)
+
+    # define network interfaces (with attached EEPROMs)
+    revpi.network_interfaces.append(LAN95XXNetworkInterface("1-5.1.2:1.0", True))
+    revpi.network_interfaces.append(LAN95XXNetworkInterface("1-5.1.2:1.0", True))
+
+    # ... or define network interfaces and determine class from type string
+    # (eg. with yaml templates or arparse)
+    LAN = find_interface_class("lan87xx")
+    revpi.network_interfaces.append(LAN('0000:00:14.3', True))
+    ####################### FROM TEMPLATE - END ###############################
+
+    # device specific parts (eg. in a loop)
+    revpi.write_hat_eeprom(image_path)
+    print(revpi.write_mac_addresses(mac))
