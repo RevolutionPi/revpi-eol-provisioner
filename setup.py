4933504c83163ef9e878e2523618dd56db0c7dac
Nicolai Buchwitz <n.buchwitz@kunbus.com>
2022-11-07 17:27:37 +0100

    Add python package details
    Add setup.py with packaging details and entry point scripts. The later
    will be installed as executable scripts among the library. Due to python
    naming conventions, the validation script was renamed to validator.py
    
    Signed-off-by: Nicolai Buchwitz <n.buchwitz@kunbus.com>

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="revpi-eol-provisioner",
+    version="1.0.0",
+    packages=find_packages(),
+    package_data={"revpi_provisioning": ["devices/*.yaml"]},
+    install_requires=[
+        "schema"
+    ],
+    entry_points={
+        "console_scripts": [
+            'revpi-eol-provisioner = revpi_provisioning.cli.provisioner:main',
+            'revpi-eol-validate-config = revpi_provisioning.cli.validator:main'
+        ]
+    }
+)
