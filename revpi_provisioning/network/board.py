631c6ccf8c38894f6d9bac21fa6e991d35b11939
Nicolai Buchwitz <n.buchwitz@kunbus.com>
2023-05-22 11:38:11 +0200

    feat: Add internal network interfaces like BCM2711
    Add network type for internal network interfaces like the BCM2711, which is used
    on the CM4 module. Even though this interface cannot be configured via eeprom
    or other means, we need it as a placeholder during our EOL provisioning.
    
    Signed-off-by: Nicolai Buchwitz <n.buchwitz@kunbus.com>

---
+++ revpi_provisioning/network/board.py
@@ -0,0 +1,19 @@
+from revpi_provisioning.network import NetworkInterface
+
+
+class BoardNetworkInterface(NetworkInterface):
+    def __init__(
+        self, device_path: str, has_eeprom: bool = False, eeprom_tool: str = None
+    ) -> None:
+        super().__init__(device_path, has_eeprom)
+
+        self.eeprom_tool = eeprom_tool
+
+
+class BCM2711NetworkInterface(BoardNetworkInterface):
+    def __init__(self) -> None:
+        super().__init__("", False)
+
+    def _write_eeprom(self, mac_address: str):
+        # Nothing to do here
+        pass
