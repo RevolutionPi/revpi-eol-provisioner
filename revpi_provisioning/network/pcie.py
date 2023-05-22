b80c097e75b1f003accff0f4c8eadc20a5202dd7
Nicolai Buchwitz <n.buchwitz@kunbus.com>
2023-05-22 11:37:33 +0200

    chore: Rename usb_path -> pcie_path
    Signed-off-by: Nicolai Buchwitz <n.buchwitz@kunbus.com>

--- revpi_provisioning/network/pcie.py
+++ revpi_provisioning/network/pcie.py
@@ -3,8 +3,8 @@ from revpi_provisioning.network.utils import find_pci_ethernet_device_name
 
 
 class PCIeNetworkInterface(NetworkInterface):
-    def __init__(self, usb_device_path: str, has_eeprom: bool = False, eeprom_tool: str = None) -> None:
-        super().__init__(usb_device_path, has_eeprom)
+    def __init__(self, pcie_device_path: str, has_eeprom: bool = False, eeprom_tool: str = None) -> None:
+        super().__init__(pcie_device_path, has_eeprom)
 
         self.eeprom_tool = eeprom_tool
 
