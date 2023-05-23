523f6516ed1481d2be40a96adb01577a9c0961b2
Nicolai Buchwitz <n.buchwitz@kunbus.com>
2023-05-23 08:42:46 +0200

    fix: Prepend local path if running from wheel
    If we are running from a wheel, add the wheel to sys.path, in order to make
    import work. Also refactor the return code handling, so that the caller of
    main() is in charge for program exit.
    
    Signed-off-by: Nicolai Buchwitz <n.buchwitz@kunbus.com>

--- revpi_provisioning/cli/utils.py
+++ revpi_provisioning/cli/utils.py
@@ -1,6 +1,8 @@
 import sys
 
 
-def error(msg: str, rc: int):
+def error(msg: str, rc: int) -> None:
+    """Print error message to STDERR and return with given return code."""
     print(msg, file=sys.stderr)
-    sys.exit(rc)
+
+    return rc
